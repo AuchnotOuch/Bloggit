b84c0f9a22d22f66b936e4ec0ff3f51b593f7bbe
Alex Auch <alexsamuelauch@gmail.com>
2022-12-22 17:37:40 -0700

    created modal to create new quote post

--- react-app/src/components/newPostBar/NewPostBar.js
+++ react-app/src/components/newPostBar/NewPostBar.js
@@ -2,22 +2,40 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import NewTextPost from './NewTextPost';
+import NewQuotePost from './NewQuotePost'
 
 const NewPostBar = () => {
     const user = useSelector(state => state.session.user)
     const [mountText, setMountText] = useState(false)
     const [mountQuote, setMountQuote] = useState(false)
     const [mountPhoto, setMountPhoto] = useState(false)
-    console.log(mountText)
+
     function mountTextModal() {
         setMountText(!mountText)
     }
+    function mountQuoteModal() {
+        setMountQuote(!mountQuote)
+    }
+    function mountPhotoModal() {
+        setMountPhoto(!mountPhoto)
+    }
+
     useEffect(() => {
 
-    }, [mountText])
+    }, [mountText, mountQuote, mountPhoto])
     return (
         <>
             {mountText &&
+                <>
+                    <NewTextPost mountText={mountText} setMountText={setMountText} />
+                </>
+            }
+            {mountQuote &&
+                <>
+                    <NewQuotePost mountQuote={mountQuote} setMountQuote={setMountQuote} />
+                </>
+            }
+            {mountPhoto &&
                 <>
                     <NewTextPost mountText={mountText} setMountText={setMountText} />
                 </>
@@ -31,7 +49,7 @@ const NewPostBar = () => {
                         <input onClick={() => mountTextModal()} type='image' src='https://www.svgrepo.com/show/372715/text.svg'></input>
                     </div>
                     <div className='new-post-option-button'>
-                        <input type='image' src='https://www.svgrepo.com/show/340879/quotes.svg'></input>
+                        <input onClick={() => mountQuoteModal()} type='image' src='https://www.svgrepo.com/show/340879/quotes.svg'></input>
                     </div>
                     <div className='new-post-option-button'>
                         <input type='image' src='https://www.svgrepo.com/show/158771/photo.svg'></input>
