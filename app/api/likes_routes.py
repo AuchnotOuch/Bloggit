8732a263af2d8e628885f691abe13d793d2f86a8
Alex Auch <alexsamuelauch@gmail.com>
2023-01-19 14:08:37 -0700

    added route to remove a like from a post

--- app/api/likes_routes.py
+++ app/api/likes_routes.py
@@ -6,7 +6,7 @@ from app.models import db, Post, User
 likes_routes = Blueprint('likes', __name__)
 
 
-@likes_routes.route('/<int:id>')
+@likes_routes.route('/post/<int:id>')
 def likes_count(id):
     '''
     Query for post, then do a count on the number of likes attributed to
@@ -20,7 +20,7 @@ def likes_count(id):
 
     return {"Error": f'Post {id} not found'}, 404
 
-@likes_routes.route('/<int:id>', methods=['POST'])
+@likes_routes.route('/post/<int:id>', methods=['POST'])
 def add_like(id):
     '''
     Query for post based on id and add the like to the join table using
@@ -35,3 +35,19 @@ def add_like(id):
         return {"message": "like added!"}
 
     return {"Error": f'Could not add like for post {id}'}, 404
+
+@likes_routes.route('/post/<int:id>', methods=['DELETE'])
+def remove_like(id):
+    '''
+    Query for post based on id and remove like from join table using
+    current user
+    '''
+
+    post = Post.query.get(id)
+    if post is not None:
+        user = current_user
+        user.post_like.remove(post)
+        db.session.commit()
+        return {"message": 'like removed!'}
+
+        return {"Error": f'Could not remove like for post {id}'}, 404
