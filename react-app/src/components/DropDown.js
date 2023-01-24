4831acb562f8109e873d22fb92cefdd3dd60d35a
Alex Auch <alexsamuelauch@gmail.com>
2023-01-24 11:57:43 -0700

    added styling to drop down

--- react-app/src/components/DropDown.js
+++ react-app/src/components/DropDown.js
@@ -1,9 +1,15 @@
 import React from "react";
+import LogoutButton from "./auth/LogoutButton";
 import './styles/NavBar.css'
 
 const DropDown = () => {
     return (
-        <div className="dropdown-container">Test</div>
+        <div className="dropdown-container">
+            <div className="menu-header">
+                <p style={{ textDecoration: 'underline' }}>Account</p>
+                <LogoutButton />
+            </div>
+        </div>
     )
 }
 
