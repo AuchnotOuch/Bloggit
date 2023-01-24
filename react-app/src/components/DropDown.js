133984b2f67b2315804633db4a6641a7f364f913
Alex Auch <alexsamuelauch@gmail.com>
2023-01-24 11:16:29 -0700

    added a drop down menu component for logged in users

---
+++ react-app/src/components/DropDown.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './styles/NavBar.css'
+
+const DropDown = () => {
+    return (
+        <div className="dropdown-container">Test</div>
+    )
+}
+
+export default DropDown
--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,12 +1,18 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
+import DropDown from './DropDown';
 import './landing/Landing.css'
+import './styles/NavBar.css'
+
 
 const NavBar = () => {
   const user = useSelector(state => state.session.user)
+  const [mountMenu, setMountMenu] = useState(false)
+
+  const toggleMenu = () => setMountMenu(!mountMenu)
 
   return (
     <nav>
@@ -37,9 +43,17 @@ const NavBar = () => {
         </>
       }
       {user &&
-        <div>
-          <LogoutButton />
-        </div>
+        <>
+          <div className='user-menu-container'>
+            <div>
+              <button onClick={toggleMenu} className='user-menu'><i className="fa-solid fa-user"></i></button>
+            </div>
+            {mountMenu &&
+              <DropDown />
+            }
+          </div>
+        </>
+
       }
 
     </nav>
